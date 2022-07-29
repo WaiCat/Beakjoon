14f417c71eaec875e8b14d48da0e4a67477dd7d5
Waicat <rxg1123@gmail.com>
2022-07-29 14:25:06 +0900

    first commit

---
+++ B1330.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class B1330 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        final int a = sc.nextInt();
+        final int b = sc.nextInt();
+
+        if(a>b)
+            System.out.println(">");
+        else if(b>a)
+            System.out.println("<");
+        else
+            System.out.println("==");
+    }
+}
