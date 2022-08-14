686d829d0c37e7011175d581ae52486b5deb0461
WaiCat <rxg1123@gmail.com>
2022-08-14 23:43:45 +0900

    22.08.14

---
+++ B10757.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.*;
+
+public class B10757 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        BigInteger sum =  a.add(b);
+        
+        System.out.println(sum);
+        sc.close();
+    }
+}
