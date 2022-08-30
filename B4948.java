92b0e01cd85bc6c5acf139f94b7d0def276e2615
WaiCat <rxg1123@gmail.com>
2022-08-30 13:53:50 +0900

    22.08.30

--- B4948.java
+++ B4948.java
@@ -10,18 +10,18 @@ public class B4948 {
                 break;
             }
             int count = 0;
-            boolean So[] = new boolean[n*2 + 1];
+            boolean So[] = new boolean[n * 2 + 1];
             So[0] = So[1] = true;
 
-            for (int i = 2; i * i <= n*2; i++) {
+            for (int i = 2; i * i <= n * 2; i++) {
                 if (!So[i]) {
-                    for (int j = i * i; j <= n*2; j += i) {
+                    for (int j = i * i; j <= n * 2; j += i) {
                         So[j] = true;
                     }
                 }
             }
 
-            for (int i = n+1; i <= n*2; i++) {
+            for (int i = n + 1; i <= n * 2; i++) {
                 if (!So[i]) {
                     count++;
                 }
