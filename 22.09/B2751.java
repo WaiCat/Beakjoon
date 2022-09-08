7d92537832fa0fae8cd12867ea71d9f0420d2d0d
Waicat <rxg1123@gmail.com>
2022-09-08 22:58:51 +0900

    22.09.08

---
+++ 22.09/B2751.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class B2751 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		System.out.println(sb);
+        sc.close();
+    }
+}
