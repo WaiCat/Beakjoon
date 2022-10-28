cf61368430cc0a1adb081de84b8b111efc683a30
Waicat <rxg1123@gmail.com>
2022-10-28 21:24:15 +0900

    22.10.28

--- 22.10/B10807.java
+++ 22.10/B10807.java
@@ -6,12 +6,12 @@ public class B10807 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr =  new int[201];
+        int[] arr = new int[201];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[Integer.parseInt(st.nextToken())+100]++;
+            arr[Integer.parseInt(st.nextToken()) + 100]++;
         }
         int m = Integer.parseInt(br.readLine());
-        System.out.println(arr[m+100]);
+        System.out.println(arr[m + 100]);
     }
 }
