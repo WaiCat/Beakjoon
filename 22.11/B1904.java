72f90d6f1be0a06b57d5b316238b798f9ffba109
Waicat <rxg1123@gmail.com>
2022-11-10 13:43:58 +0900

    22.11.10

--- 22.11/B1904.java
+++ 22.11/B1904.java
@@ -11,14 +11,10 @@ public class B1904 {
         arr[0] = 1;
         arr[1] = 2;
 
-        countnum();
-
-        System.out.println(arr[n-1] % 15746);
-    }
-
-    static void countnum() {
         for (int i = 2; i < n; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
+            arr[i] = (arr[i-1] + arr[i-2]) % 15746;
         }
+
+        System.out.println(arr[n-1]);
     }
 }
---
+++ 22.11/B9184.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class B9184 {
+    static int[][][] arr = new int[21][21][21];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+        }
+    }
+
+    public static int w(int a, int b, int c) {
+
+        if(0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20 && arr[a][b][c] != 0){
+            return arr[a][b][c];
+        }
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+
+        if (a < b && b < c) {
+            return arr[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        }
+
+        return arr[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+    }
+}
