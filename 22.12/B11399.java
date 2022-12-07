e0ee5a3d4cd945dcfc7228f789f99d22e223f089
Waicat <rxg1123@gmail.com>
2022-12-08 00:02:49 +0900

    22.12.08

---
+++ 22.12/B11399.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class B11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int sum = arr[0];
+        for (int i = 1; i < n; i++) {
+            arr[i] += arr[i - 1];
+            sum += arr[i];
+        }
+
+        System.out.println(sum);
+    }
+}
