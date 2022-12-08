c17aa2b6e0ae46df3b962ba933f06884521ad4da
Waicat <rxg1123@gmail.com>
2022-12-09 00:24:32 +0900

    22.12.09

---
+++ 22.12/B10828.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class B10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[10001];
+
+        int size = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String str = st.nextToken();
+
+            if (str.equals("push")) {
+                arr[size] = Integer.parseInt(st.nextToken());
+                size++;
+            } else if (str.equals("top")) {
+                if (size == 0) {
+                    System.out.println(-1);
+
+                } else {
+                    System.out.println(arr[size-1]);
+                }
+            } else if (str.equals("pop")) {
+                if (size == 0) {
+                    System.out.println(-1);
+
+                } else {
+                    System.out.println(arr[size-1]);
+                    size--;
+                }
+            } else if (str.equals("size")) {
+                System.out.println(size);
+            } else if (str.equals("empty")) {
+                if (size == 0) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            }
+        }
+    }
+}
