8cce089a01106f8e34e03a467343958fe19df8dd
WaiCat <rxg1123@gmail.com>
2023-09-04 22:49:42 +0900

    23.09.04

---
+++ 23.09/B10810.py
@@ -0,0 +1,14 @@
+num, r = map(int, input().split())
+
+arr = []
+
+for i in range(num):
+    arr.append(0)
+
+for i in range(r):
+    n1, n2, n3=map (int,input().split())
+    for j in range(n1-1, n2):
+        arr[j] = n3
+
+for i in range(num):
+    print(int(arr[i]), end=" ")
