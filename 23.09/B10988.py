7f336f16091bf93ed291247235931e1584bb881d
WaiCat <rxg1123@gmail.com>
2023-09-06 13:45:32 +0900

    23.09.06

---
+++ 23.09/B10988.py
@@ -0,0 +1,13 @@
+str = input()
+
+bool = True
+
+for i in range(int(len(str)/2)):
+    if(str[i]!=str[-i-1]):
+        bool = False
+        break
+    
+if(bool):
+    print(1)
+else:
+    print(0)
