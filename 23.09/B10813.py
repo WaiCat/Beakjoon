84cb93ce83e468a54714ea02b647fd9d31d4b68f
WaiCat <rxg1123@gmail.com>
2023-09-06 13:45:15 +0900

    23.09.06

---
+++ 23.09/B10813.py
@@ -0,0 +1,15 @@
+num, r = map(int, input().split())
+
+arr = []
+
+for i in range(num):
+    arr.append(i+1)
+
+for i in range(r):
+    n1, n2=map (int,input().split())
+    tem = arr[n1-1]
+    arr[n1-1] = arr[n2-1]
+    arr[n2-1] = tem
+
+for i in range(num):
+    print(int(arr[i]), end=" ")
