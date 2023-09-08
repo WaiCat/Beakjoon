bfa5f4d794634c58c529bb2d6863a45ef781dc84
WaiCat <rxg1123@gmail.com>
2023-09-09 00:24:44 +0900

    23.09.09

---
+++ 23.09/B2346.py
@@ -0,0 +1,27 @@
+from sys import stdin
+from collections import deque
+
+n = int(stdin.readline())
+m = list(map(int, stdin.readline().split()))
+d = deque()
+q = deque()
+
+answer = []
+
+for i in range(n):
+    d.append(i+1)
+    q.append(m[i])
+
+for i in range(n):
+    answer.append(str(d.popleft()))
+    num = q.popleft()
+    if(num > 0):
+        num = -(num-1)
+    else:
+        num = -num
+            
+    d.rotate(num)
+    q.rotate(num)
+
+        
+print(" ".join(answer))
