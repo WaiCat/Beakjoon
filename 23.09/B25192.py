27621229447a395bf6c1eff4ada797254d2ff26c
WaiCat <rxg1123@gmail.com>
2023-09-09 13:19:07 +0900

    2023.09.09

---
+++ 23.09/B25192.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+n = int(stdin.readline())
+
+lists = set()
+total = 0
+
+for _ in range(n):
+    str = stdin.readline().rstrip()
+    if(str == "ENTER"):
+        lists.clear()
+    elif(str not in lists):
+        total += 1
+        lists.add(str)
+    
+print(total)
