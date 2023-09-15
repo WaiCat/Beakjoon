31447e02275eaf5be1e7064ecc7458c878af5cef
WaiCat <rxg1123@gmail.com>
2023-09-15 18:27:46 +0900

    2023.09.15

--- 23.09/B9251.py
+++ 23.09/B9251.py
@@ -1,15 +1,14 @@
 word1 = list(input())
 word2 = list(input())
 
-m = min(len(word1), len(word2))
-
 dp = [0]*len(word2)
 
+arr = []
+
 for i in range(len(word1)):
+    arr.append([])
     for j in range(len(word2)):
         if (word1[i] == word2[j]):
-            dp[j] += 1
-        elif (j != 0 and dp[j-1] > dp[j]):
-            dp[j] = dp[j-1]
+            arr[i].append(j)
 
 print(max(dp))
