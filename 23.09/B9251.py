df798619fc99cc7911b224c81798ea7a7ef3ae45
WaiCat <rxg1123@gmail.com>
2023-09-16 15:22:37 +0900

    23.09.16

--- 23.09/B9251.py
+++ 23.09/B9251.py
@@ -1,14 +1,13 @@
 word1 = list(input())
 word2 = list(input())
 
-dp = [0]*len(word2)
+dp = [[0 for j in range(len(word2)+1)] for i in range(len(word1)+1)]
 
-arr = []
+for i in range(1, len(word1)+1):
+    for j in range(1, len(word2)+1):
+        if (word1[i-1] == word2[j-1]):
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
 
-for i in range(len(word1)):
-    arr.append([])
-    for j in range(len(word2)):
-        if (word1[i] == word2[j]):
-            arr[i].append(j)
-
-print(max(dp))
+print(max(max(dp)))
