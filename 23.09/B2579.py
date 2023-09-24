91746fc9279591a7e4197a331f848978534bc218
WaiCat <rxg1223@naver.com>
2023-09-24 13:16:08 +0900

    23.09.24

---
+++ 23.09/B2579.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
+    
+dp = []
+
+dp.append(arr[0])
+dp.append(arr[0]+arr[1])
+dp.append(max(arr[0], arr[1])+arr[2])
+
+for i in range(3, n):
+    dp.append(max(dp[i-2], dp[i-3]+arr[i-1])+arr[i])
+    
+print(max(dp))
