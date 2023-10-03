6462bf75980d3eedd87ee7941845b52107315ed1
WaiCat <rxg1123@gmail.com>
2023-10-03 23:02:31 +0900

    23.10.03

---
+++ 23.10/B1780.py
@@ -0,0 +1,45 @@
+n = int(input())
+
+arr = []
+
+mone = 0
+zero = 0
+one = 0
+
+
+def check(a, b, c, d):
+    global mone, zero, one
+
+    tem = []
+    row = b - a
+
+    for i in range(c, d):
+        tem.append(arr[i][a:b])
+
+    array_sum = sum(sum(row) for row in tem)
+
+    if array_sum == row*row:
+        one += 1
+    elif array_sum == -row*row:
+        mone += 1
+    else:
+        m = int(row/3)
+        check(a, a+m, c, c+m)
+        check(a, a+m, c+m, d-m)
+        check(a, a+m, d-m, d)
+        check(a+m, b-m, c, c+m)
+        check(a+m, b-m, c+m, d-m)
+        check(a+m, b-m, d-m, d)
+        check(b-m, b, c, c+m)
+        check(b-m, b, c+m, d-m)
+        check(b-m, b, d-m, d)
+
+
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+check(0, n, 0, n)
+
+print(mone)
+print(zero)
+print(one)
