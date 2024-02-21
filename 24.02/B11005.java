82b9b185126a8a39b54699f92698c56ac900167a
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-02-21 16:56:14 +0900

    Create B11005.java

---
+++ 24.02/B11005.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class B11005 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+
+    Deque<String> d = new ArrayDeque<>();
+
+    while (n != 0) {
+      int tmp = n % b;
+      if (tmp > 10) {
+        tmp += 55;
+        d.addFirst(Character.toString(tmp));
+      } else {
+        d.addFirst(Integer.toString(tmp));
+      }
+      n = n / b;
+    }
+    for (String element : d) {
+      System.out.print(element);
+    }
+  }
+}
