d5fdfc883ebb858e7f856bdc2c97bd196fd6a994
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-03-01 14:27:58 +0900

    Create B1011.java

---
+++ 24.02/B1011.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class B1011 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int t = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < t; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+
+      int num = y - x;
+
+      int border = (int) Math.sqrt(num);
+
+      if (num <= border * (border + 1)) {
+        if (num <= border * border) {
+          System.out.println(border * 2 - 1);
+        } else {
+          System.out.println(border * 2);
+        }
+      } else {
+        border++;
+        if (num <= border * border) {
+          System.out.println(border * 2 - 1);
+        } else {
+          System.out.println(border * 2);
+        }
+      }
+    }
+  }
+}
